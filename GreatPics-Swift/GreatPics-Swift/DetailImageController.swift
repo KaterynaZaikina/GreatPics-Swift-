ddf3b21fb40d11a0c520bf48bc8799b7b190cde6
Kateryna Zaikina <kateryna.zaikina@gmail.com>
2015-12-04 19:27:56 +0200

    1) Added scrollView and zooming

--- GreatPics-Swift/GreatPics-Swift/DetailImageController.swift
+++ GreatPics-Swift/GreatPics-Swift/DetailImageController.swift
@@ -9,20 +9,21 @@
 import Foundation
 import UIKit
 
-class DetailImageController: UIViewController {
-
+class DetailImageController: UIViewController, UIScrollViewDelegate {
+    
     @IBOutlet weak var image: InstaPostView!
+    @IBOutlet weak var scrollView: UIScrollView!
     var postImageURL: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if let url = postImageURL {
-        self.image.loadImageWithURL(NSURL(string: url), placeholderImage: UIImage(named: "placeholder.png")!)
+            self.image.loadImageWithURL(NSURL(string: url), placeholderImage: UIImage(named: "placeholder.png")!)
         }
     }
     
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return self.image
+    }
     
-    
-
-
 }
